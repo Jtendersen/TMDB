4ae55c04bef77c3083acdee9cc8e23f42638bcc2
Jtendersen <jtendersen@hotmail.com>
2022-11-28 20:37:18 -0300

    replaced uri

--- api/config/config.js
+++ api/config/config.js
@@ -6,6 +6,16 @@ const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "1234";
 const DB_NAME = process.env.DB_NAME || "users_tmdb";
+const DB_URI =
+  process.env.DB_URI ||
+  (DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  {
+    host: DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  });
 
 module.exports = {
   PORT,
@@ -13,4 +23,5 @@ module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
+  DB_URI,
 };
--- api/db.js
+++ api/db.js
@@ -4,13 +4,10 @@ const {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
+  DB_URI,
 } = require("./config/config.js");
 console.log("this is de db_host", DB_HOST);
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = new Sequelize(DB_URI);
 
 module.exports = sequelize;
