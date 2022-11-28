bba0bbe236538755294c9b1b1c9bd55c708a7d0e
Jtendersen <jtendersen@hotmail.com>
2022-11-28 07:44:59 -0300

    proxy deleted to deploy

--- api/db.js
+++ api/db.js
@@ -5,6 +5,7 @@ const {
   DB_PASSWORD,
   DB_HOST,
 } = require("./config/config.js");
+console.log("this is de db_host", DB_HOST);
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
